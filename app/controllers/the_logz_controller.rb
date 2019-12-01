f6895f8ceb2390ac56dc036455ae6f478896d20a
Eric Boersma <eric.boersma@gmail.com>
2019-12-01 11:10:00 -0500

    [CH9] Auto-refreshing the_logz every ten seconds to load new data

--- app/controllers/the_logz_controller.rb
+++ app/controllers/the_logz_controller.rb
@@ -1,5 +1,6 @@
 class TheLogzController < ApplicationController
   def index
+    response.set_header("REFRESH", 10)
     @log_entries = RequestLog.all_logs
     @most_popular = Population.most_popular_years
   end
